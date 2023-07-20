503b86ff5b6e926ac27e5e2c93d7a7cab9df5f07
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-20 22:33:19 +0900

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Header } from "./component/Header";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    < >
+  <Header />
+    </>
   );
 }
 
---
+++ src/component/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const NavigationBar = styled.nav`
+    width: 100wh;
+    height: 70px;
+    background-color: #015ad0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+
+    color: #fff;
+    font-size: 24px;
+    font-weight: 500;
+`
+
+export const Header = () => {
+  return (
+    <NavigationBar>
+        
+    </NavigationBar>
+  )
+}
