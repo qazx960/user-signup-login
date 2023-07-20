18643cad6aaa0cd0de82504d502025f540a4993b
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-20 23:13:03 +0900

    axios installed and GET data confirmed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./component/Header";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    axios.get(' https://api.escuelajs.co/api/v1/users')
+    .then((data) => {
+      console.log(data.data)
+      setUser(data.data)
+    })
+  },[])
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login user={user} />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,47 +1,62 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from "react";
+import { styled } from "styled-components";
+
+
+
+
+export const Login = ({user}) => {
+
+  return (
+    <LoginContainer>
+      <h1>Login</h1>
+      <Form>
+        <label htmlFor="username">Username</label>
+        <Input type="text" id="username" />
+        <label htmlFor="password">Password</label>
+        <Input type="password" id="password" />
+        <Button >Login</Button>
+      </Form>
+    </LoginContainer>
+  );
+};
 
 const LoginContainer = styled.section`
   border: 1px solid black;
 
+  height: 400px;
+  width: 350px;
+  margin: 100px auto;
 
-height: 350px;
-width: 350px;
-margin: 100px auto;
-
-border-radius: 12px;
+  border-radius: 12px;
 
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
-
-`
+`;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around;
-  height: 120px;
-`
+  justify-content: space-evenly;
+  height: 200px;
+`;
 const Input = styled.input`
-  width: 200px;
-  height: 24px;
+  width: 240px;
+  height: 28px;
   border-radius: 3px;
-  border-color: rgba(0,0,0,0.2);
+  border-color: rgba(0, 0, 0, 0.2);
   padding: 3px;
-`
-export const Login = () => {
-  return (
-    <LoginContainer>
-      <h1>Login</h1>
-      <Form>
-          <label htmlFor="">Username</label>
-          <Input type="text"  />
-          <label htmlFor="">Password</label>
-          <Input type="password"  />
-      </Form>
-    </LoginContainer>
-  )
-}
+`;
+
+const Button = styled.button`
+  width: 200px;
+  border-color: rgba(0, 0, 0, 0.2);
+  height: 30px;
+  margin-top: 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
