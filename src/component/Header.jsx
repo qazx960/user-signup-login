901ecaedef3e30006f4350d3f296f068f928a7a2
Jiwon Lee <giwon.yi339@gmail.com>
2023-07-20 22:41:28 +0900

    Header markup and css

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from "react";
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
 
 const NavigationBar = styled.nav`
-    width: 100wh;
-    height: 70px;
-    background-color: #015ad0;
+  width: 100wh;
+  height: 70px;
+  background-color: #015ad0;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 40px;
 
-    color: #fff;
-    font-size: 24px;
-    font-weight: 500;
-`
+  color: #fff;
+  font-size: 24px;
+  font-weight: 500;
+`;
+
+const NavItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 40%;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+`;
 
 export const Header = () => {
   return (
     <NavigationBar>
-        
+      <NavItems>
+        <div>Home</div>
+        <div>Login</div>
+        <div>Signup</div>
+      </NavItems>
     </NavigationBar>
-  )
-}
+  );
+};
